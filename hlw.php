5279c354200242b54feb959f18a29765b3438d08
creativethemax <creativethemax@gmail.com>
2022-11-19 22:16:37 +0600

    sio

--- hlw.php
+++ hlw.php
@@ -3,4 +3,6 @@ git config --global user.name "w3schools-test"
 git config --global user.email "test@w3schools.com"
 git add .
 git commit -m "sio"
-git push 
+git branch -M main
+git remote add origin https://github.com/creativethemax/as.git
+git push
